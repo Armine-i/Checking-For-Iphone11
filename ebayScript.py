bebaf7286b261f5fab8bf00d39b116b3f54c4f2f
Mina <armirad@yahoo.ca>
2020-02-16 21:49:20 -0500

    saving items to only send one notification

--- ebayScript.py
+++ ebayScript.py
@@ -2,16 +2,25 @@ from lxml import html
 from twilio.rest import Client
 import requests
 
+# url of Ebay seller's store
 page = requests.get('https://www.ebay.ca/sch/indiestartup/m.html?_nkw=&_armrs=1&_ipg=&_from=')
 tree = html.fromstring(page.content)
 
+# xpath of html element containing items being sold
 items = tree.xpath('//a[@class="vip"]/text()')
+# list where we save items we already know about
+items_list = []
 
+# Twilio credentials
 account_sid = 'AC854b91ea15ddb0552b5fbff4de2827e6'
 auth_token = 'TWILIO_TOKEN'
 client = Client(account_sid, auth_token)
 
 while True:
   for i in items:
-    if "11" in i:
+    # Check if it's a new item and search for "11" for iPhone 11
+    if i not in items_list and "11" in i:
+      # Send me a text
       message = client.messages.create(body=items, from_='+15149006687', to='+15149963923')
+    # save item
+    items_list.append(i)
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from subprocess import Popen
 import sys
 
 filename = "ebayScript.py"
+# run and relaunch my script if there's an exception
 while True:
     print("\nStarted " + filename)
     p = Popen("python3 " + filename, shell=True)
