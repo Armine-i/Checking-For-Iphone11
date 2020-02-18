369d0a1f98977a335fcb18f720548a4bab37b6d2
Mina <armirad@yahoo.ca>
2020-02-17 21:01:42 -0500

    added pausing for 1 minute

--- ebayScript.py
+++ ebayScript.py
@@ -1,13 +1,12 @@
 from lxml import html
 from twilio.rest import Client
 import requests
+import time
 
 # url of Ebay seller's store
 page = requests.get('https://www.ebay.ca/sch/indiestartup/m.html?_nkw=&_armrs=1&_ipg=&_from=')
 tree = html.fromstring(page.content)
 
-# xpath of html element containing items being sold
-items = tree.xpath('//a[@class="vip"]/text()')
 # list where we save items we already know about
 items_list = []
 
@@ -17,6 +16,8 @@ auth_token = 'TWILIO_TOKEN'
 client = Client(account_sid, auth_token)
 
 while True:
+  # xpath of html element containing items being sold
+  items = tree.xpath('//a[@class="vip"]/text()')
   for i in items:
     # Check if it's a new item and search for "11" for iPhone 11
     if i not in items_list and "11" in i:
@@ -24,3 +25,4 @@ while True:
       message = client.messages.create(body=items, from_='+15149006687', to='+15149963923')
     # save item
     items_list.append(i)
+  time.sleep(60)
